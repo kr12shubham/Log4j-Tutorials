017fdfbd8f07d56956f0402267aadcfd904a9a9d
Shubham Kumar <kumarershubam@gmail.com>
2020-12-13 23:48:43 +0530

    First Commit

---
+++ src/Alpha/Demo1.java
@@ -0,0 +1,15 @@
+package Alpha;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Demo1 {
+	
+	public static Logger log= LogManager.getLogger(Demo1.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
